23f680fad42dce30fe336e635d0b94a1c80325a9
dudpray0220 <qodudgus0220@naver.com>
2021-12-21 18:11:40 +0900

    modified

---
+++ Ch 5/printMinMax/printMinMax/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace printMinMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 5개의 수 입력받음
+            int[] numbers = new int[5]; // 배열 생성
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("input number : ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            // 배열 출력 테스트
+            foreach (var number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+
+            // Min, Max print
+            Console.WriteLine("\nMax value : " + numbers.Max());
+            Console.WriteLine("Min value : " + numbers.Min());
+        }
+    }
+}
---
+++ Ch 5/star3(pyramid)/star3(pyramid)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace star3_pyramid_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The longest star : 15 star
+            for (int i = 0; i < 16; i = i + 2) // i : 0, 2, 4
+            {
+                for (int j = 0; j < 16 - i; j=j+2) // j : 0, 2, 4              j < 16, 14, 12
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k < i+1; k++) // k ; 0 1 2          k < 1, 3, 5 
+                {
+                Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
