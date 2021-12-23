a229203af7ad0b56e0984749c0111cb13409a5d3
dudpray0220 <qodudgus0220@naver.com>
2021-12-23 17:54:09 +0900

    ch 7

---
+++ Ch 7/Shadowing/Shadowing/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Shadowing
+{
+    class Program
+    {
+        public static int number = 10;
+        static void Main(string[] args)
+        {
+            int number = 20;
+            Console.WriteLine(number);
+        }
+    }
+}
---
+++ Ch 7/hiding/hiding/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace hiding
+{
+    class Program
+    {
+        class Parent
+        {
+               public void Method()
+            {
+                Console.WriteLine("parent");
+            }
+        }
+
+        class Child : Parent
+        {
+            public void Method()
+            {
+                Console.WriteLine("child");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Child child = new Child();
+            child.Method();
+            ((Parent)child).Method();
+        }
+    }
+}
