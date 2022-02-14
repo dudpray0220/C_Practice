4c9f4aa842d503d9bef6c075b25c40bf9991af8b
dudpray0220 <qodudgus0220@naver.com>
2022-02-14 22:04:36 +0900

    installShield Test

---
+++ InstallShield/MyProject1/MyProject1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyProject1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
